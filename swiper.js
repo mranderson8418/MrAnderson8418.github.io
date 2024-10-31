cde317cd1272915c3417d4dd3695a746ec003e16
mranderson8418 <mranderson8418@gmail.com>
2024-10-30 23:37:00 -0400

    commit

--- swiper.js
+++ swiper.js
@@ -1,9 +1,36 @@
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "vertical",
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: ".swiper-scrollbar",
+	},
+});
+
 function next() {
 	var myImage = new Array();
-	myImage[0] = "01_SCRUBLANDS_WEBPAGE_SMA/SOAP_PICS/MARINE_GREEN_PNG/MarineGreen01.png";
-	myImage[1] = "desert.jpg";
-	myImage[2] = "jellyfish.jpg";
-	myImage[3] = "flower.jpg";
+	myImage[0] = "/SOAP_PICS/MARINE_GREEN_JPG/MARINE_GREEN_new_1.jpeg";
+	myImage[1] = "/SOAP_PICS/MARINE_GREEN_JPG/MARINE_GREEN_new_2.jpeg";
+	myImage[2] = "/SOAP_PICS/MARINE_GREEN_JPG/MARINE_GREEN_new_3.jpegg";
+	myImage[3] = "/SOAP_PICS/MARINE_GREEN_JPG/MARINE_GREEN_new_4.jpeg";
 
 	ImageCnt++;
 
