6c99712f626385921a40d4fa6b26a943b3f47866
scottmichaelanderson8418 <scottmichaelanderson8418@gmail.com>
2024-12-12 14:53:16 -0500

    commit

--- js/DropDownContents.js
+++ js/DropDownContents.js
@@ -1,14 +1,3 @@
-// const input = document.getElementById("menuButton");
-// const dropdown = document.getElementById("dropDownMenu");
-
-// input.addEventListener("input", () => {
-// 	dropdown.hidden = !dropdown.hidden;
-// });
-
-// input.addEventListener("touchstart", () => {
-// 	dropdown.hidden = !dropdown.hidden;
-// });
-
 const dropdown = document.getElementById("menuButton");
 const dropdownButton = document.getElementById("dropDownMenu");
 
